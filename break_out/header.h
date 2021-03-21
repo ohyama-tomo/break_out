a45b70ad503c12d08b97f05c2e067bedd61547d5
ohyama_tomo <ohyama@iplab.cs.tsukuba.ac.jp>
2021-03-21 17:01:01 +0900

    first from laptop

--- break_out/header.h
+++ break_out/header.h
@@ -8,10 +8,3 @@
 #include <iostream>
 void LoadSE(MCI_OPEN_PARMS open, MCI_PLAY_PARMS play, const char* resource);
 void PlaySE(MCIDEVICEID id, MCI_PLAY_PARMS play);
-std::wstring String2WString(std::string s){
-	int bufsize = MultiByteToWideChar(CP_ACP, 0, s.c_str(), -1, (wchar_t*)NULL, 0);
-	wchar_t* buffer = new wchar_t[bufsize];
-	std::wstring result(buffer, buffer+bufsize-1);
-	delete[] buffer;
-	return result;
-}
